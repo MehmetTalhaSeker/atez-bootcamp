ec36be6d02b972b51226bec711ba5d34d314beeb
mehmettalhaseker <mtalhaseker22@gmail.com>
2023-02-24 07:39:04 +0300

    Initial commit - Completed first task(infinite args).

---
+++ day-1/01-infinite-args.test.ts
@@ -0,0 +1,38 @@
+import { describe, expect, it } from "vitest";
+
+/**
+ * A function that accepts infinite number of "numbers".
+ * @param numbers only accepts "number" type.
+ */
+export const addNumbers = (...numbers: number[]) => {
+  return numbers.reduce((acc, curr) => {
+    if (typeof curr !== "number") {
+      throw new Error("Function parameters can only be numbers!");
+    }
+    return acc + curr;
+  }, 0);
+};
+
+describe("addNumbers tests", () => {
+  it("should successfully add numbers", function () {
+    expect(addNumbers(6, 5)).toEqual(11);
+    expect(addNumbers(6)).toEqual(6);
+    expect(addNumbers(6, 5, 4)).toEqual(15);
+    expect(addNumbers(-6, 5, 4)).toEqual(3);
+    expect(addNumbers(5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5)).toEqual(65);
+    expect(addNumbers()).toEqual(0);
+  });
+
+  it("should fail", () => {
+    // @ts-expect-error
+    expect(() => addNumbers("12", 12, "abc")).toThrowError();
+    // @ts-expect-error
+    expect(() => addNumbers(12, "abc")).toThrowError();
+    // @ts-expect-error
+    expect(() => addNumbers(12, "12")).toThrowError();
+    // @ts-expect-error
+    expect(() => addNumbers(true, false)).toThrowError();
+    // @ts-expect-error
+    expect(() => addNumbers([], false)).toThrowError();
+  });
+});
